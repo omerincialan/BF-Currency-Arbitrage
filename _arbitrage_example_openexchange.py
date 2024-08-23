a07eb5972de0fec325af858998172bbf01a1344a
Ömer Incialan <omerincalan@gmail.com>
2024-08-22 23:51:45 -0700

    Update _arbitrage_example_openexchange.py

--- _arbitrage_example_openexchange.py
+++ _arbitrage_example_openexchange.py
@@ -9,14 +9,14 @@ from math import log
 import networkx as nx
 import matplotlib.pyplot as plt
 
-currencies = ( 'EUR', 'USD', 'MXN', 'CAD', 'AUD','CHF', 'TRY')
-api_key = 'fe69bd3037f74f699cf2bf8070b44374'
+currencies = ('CAD', 'EUR', 'USD', 'RUB', 'CHF', 'TRY', 'MXN') # defining a tuple here
+api_key = 'bfdab30d40bd42c3b12a4e5e9e40898a'
 
 def get_rates(currencies: Tuple[str, ...], api_key: str) -> List[List[float]]:
-    rates = []
-    response = requests.get(f'https://openexchangerates.org/api/latest.json?app_id={api_key}')
-    data = response.json()['rates']
-    rates_to_usd = {currency: data.get(currency, 0) for currency in currencies}
+    rates = [] # initializing an empty list
+    response = requests.get(f'https://openexchangerates.org/api/latest.json?app_id={api_key}') #sending a get request
+    data = response.json()['rates'] # we are extracting the json response  here in this list
+    rates_to_usd = {currency: data.get(currency, 0) for currency in currencies} # creationg another dictionarty where we keep each currency's rate against US dollar
 
     for currency in currencies:
         rates.append([rates_to_usd[currency] / rates_to_usd[curr] if rates_to_usd[curr] != 0 else 0 for curr in currencies])
@@ -24,9 +24,37 @@ def get_rates(currencies: Tuple[str, ...], api_key: str) -> List[List[float]]:
 
 def negate_logarithm_convertor(graph: Tuple[Tuple[float]]) -> List[List[float]]:
     ''' log of each rate in graph and negate it'''
+
     result = [[-log(edge) if edge != 0 else float('inf') for edge in row] for row in graph]
     return result
 
+# This function takes tuple of currencies, matrix of exchange rates and arbitrage paths we derive from another function
+# we use built in graph library of python here.
+def visualize_graph(currencies: Tuple[str, ...], rates: List[List[float]], arbitrage_paths: List[List[int]]):
+    G = nx.DiGraph()
+    n = len(currencies)
+
+    for i in range(n):
+        for j in range(n):
+            if rates[i][j] != 0 and rates[i][j] != float('inf'):
+                G.add_edge(currencies[i], currencies[j], weight=rates[i][j])
+
+    pos = nx.spring_layout(G)
+    edge_labels = nx.get_edge_attributes(G, 'weight')
+
+    plt.figure(figsize=(12, 8))
+    nx.draw(G, pos, with_labels=True, node_size=2000, node_color="skyblue", font_size=15, font_weight="bold")
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
+
+    for path in arbitrage_paths:
+        edges = [(currencies[path[i]], currencies[path[i + 1]]) for i in range(len(path) - 1)]
+        nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color='green', width=2)
+
+    plt.title("Currency Exchange Graph and Arbitrage Opportunities")
+    plt.show()
+
+# this is the main loop, where we apply bellman-ford algorithm on the graph.
+# I have another video where I explain the working logic of BF. If you are into algorithms you can have a look at it.
 def arbitrage(currency_tuple: tuple, rates_matrix: Tuple[Tuple[float, ...]]) -> List[List[int]]:
     ''' Calculates arbitrage situations and prints out the details of this calculations'''
 
@@ -66,31 +94,9 @@ def arbitrage(currency_tuple: tuple, rates_matrix: Tuple[Tuple[float, ...]]) -> 
 
     return arbitrage_paths
 
-
-def visualize_graph(currencies: Tuple[str, ...], rates: List[List[float]], arbitrage_paths: List[List[int]]):
-    G = nx.DiGraph()
-    n = len(currencies)
-
-    for i in range(n):
-        for j in range(n):
-            if rates[i][j] != 0 and rates[i][j] != float('inf'):
-                G.add_edge(currencies[i], currencies[j], weight=rates[i][j])
-
-    pos = nx.spring_layout(G)
-    edge_labels = nx.get_edge_attributes(G, 'weight')
-
-    plt.figure(figsize=(12, 8))
-    nx.draw(G, pos, with_labels=True, node_size=2000, node_color="skyblue", font_size=15, font_weight="bold")
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='red')
-
-    for path in arbitrage_paths:
-        edges = [(currencies[path[i]], currencies[path[i + 1]]) for i in range(len(path) - 1)]
-        nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color='green', width=2)
-
-    plt.title("Currency Exchange Graph and Arbitrage Opportunities")
-    plt.show()
-
 if __name__ == "__main__":
     rates = get_rates(currencies, api_key)
     arbitrage_paths = arbitrage(currencies, rates)
     visualize_graph(currencies, rates, arbitrage_paths)
+    # here we are calling all the functions.
+    # we could improve the code so that it prints the arbotrage amount. I mean, If we buy 10K with how much money we end up.
